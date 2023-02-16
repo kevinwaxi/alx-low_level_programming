0d63c218ef597be06c392a837e6537e19c8cc369
kevin waxi <kevinwakhisi70@gmail.com>
2023-02-16 12:20:53 -0800

    build::constrauction: Completed assignment 6
    completed assignment 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	long int b ;
+	long long int c;
+	char d;
+	float f;
+
+	printf("size of char: %lu byte(s)\n", (unsigned long)size of (d));
+	printf("size of an int: %lu byte(s)\n", (unsigned long)size of (a));
+	printf("size of long int: %lu byte(s)\n", (unsigned long)size of (b));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)size of (c));
+	printf("size of float: %lu byte(s)\n", (unsigned long)size of (f));
+	return (0);
+}
