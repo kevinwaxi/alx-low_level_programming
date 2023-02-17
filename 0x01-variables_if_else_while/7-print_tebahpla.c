55e296d35bf29afd28f0e8f1c2739e521877727c
kevin waxi <kevinwakhisi70@gmail.com>
2023-02-17 12:07:02 -0800

    updated asignment 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
  *
+ * Description: print a-z in reverse
+ *
  * Return: Always 0 (Success)
- */
+*/
 
 int main(void)
 {
-	char low;
+	char ch = 'z';
 
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		--ch;
+	}
+	putchar('\n');
 
 	return (0);
 }
