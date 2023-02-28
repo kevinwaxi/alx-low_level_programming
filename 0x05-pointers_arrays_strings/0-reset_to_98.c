eb1a7ab5db6740325280f123295fdd827cf3a204
kevin waxi <kevinwakhisi70@gmail.com>
2023-02-27 21:02:07 -0800

    build: :construction: Completed task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of n to 98
+ * @n: integer pointer to a variable
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
